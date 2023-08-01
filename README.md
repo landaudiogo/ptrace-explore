6ae35c13b0a921171eccd0aa9ccfc2866676324d
Diogo Landau <diogo.hewitt.landau@hotmail.com>
2023-08-01 09:05:10 +0100

    Add set tracee registers
    The tracer was used to set the tracee's rip register which allowed any
    code within the tracee to be executed. The address is computed based on
    the base map address for the text section from
    `/proc/{tracee_pid}/maps` and the instruction offset we want to set the
    rip at.

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ tracer process to control a tracee program.
 The milestones in this project are: 
 
 - [x] Attach to a running process;
-- [ ] Read & save a process' registers (PEEK);
-- [ ] Set the registers and write data to the process' address space (POKE);
+- [x] Read & save a process' registers (PEEK);
+- [x] Set the registers and write data to the process' address space (POKE);
 - [ ] Execute a simple syswrite system call;
 - [ ] Restore the registers to their previous state & continue execution;
--- src/simple-attach/src/main.c
+++ src/simple-attach/src/main.c
@@ -6,17 +6,45 @@
 #include <sys/user.h>
 #include <sys/wait.h>
 
-void read_registers(pid_t tracee) {
+struct user_regs_struct 
+read_regs(pid_t tracee) {
     struct user_regs_struct regs;
+    memset(&regs, 0, sizeof(struct user_regs_struct));
 
     ptrace(PTRACE_GETREGS, tracee, NULL, &regs);
-    unsigned instr = ptrace(PTRACE_PEEKTEXT, tracee, regs.rip, 0);
-    printf("RIP = 0x%08llx. instr = 0x%08x\n", 
-           regs.rip, instr);
+
+    return regs;
 }
 
-void run_attach(pid_t tracee) 
+void
+print_reverse_word(unsigned long word) {
+    for (int i=0; i<8; i++) {
+        printf("%02x ", (unsigned char) (word >> i*8) & (0xff));
+    }
+    printf("\n");
+}
+
+void 
+read_data(pid_t tracee, unsigned long address, size_t size) { 
+    unsigned long data;
+    printf("data at address: 0x%08lx\n", address);
+    for (int i=0; i < size; i+=8) {
+        data = ptrace(PTRACE_PEEKDATA, tracee, address, NULL);
+        print_reverse_word(data);
+    }
+}
+
+void 
+set_regs(pid_t tracee, struct user_regs_struct *regs) {
+    ptrace(PTRACE_SETREGS, tracee, NULL, regs);
+}
+
+void run_attach(pid_t tracee, unsigned long map_address) 
 {
+    struct user_regs_struct old_regs;
+    struct user_regs_struct new_regs; 
+    memset(&new_regs, 0, sizeof(struct user_regs_struct));
+
     if (ptrace(PTRACE_ATTACH, tracee, NULL, NULL) < 0) {
         perror("Failed to attach to process\n");
         exit(1);
@@ -29,7 +57,17 @@ void run_attach(pid_t tracee)
         waitpid(tracee, &wait_status, WUNTRACED);
         if (!WIFSTOPPED(wait_status))
             break;
-        read_registers(tracee);
+
+        if (icounter == 0) {
+            old_regs = read_regs(tracee);
+            printf("0x%llx\n", old_regs.rip);
+            memcpy(&new_regs, &old_regs, sizeof(struct user_regs_struct));
+            // new_regs.rip = map_address + 0x72;
+            new_regs.rip = old_regs.rip; // - 0x5e;
+            read_data(tracee, new_regs.rip, 0x5);
+            set_regs(tracee, &new_regs);
+            getchar();
+        }
 
         if (ptrace(PTRACE_SINGLESTEP, tracee, NULL, NULL) < 0) {
             perror("Failed to single step");
@@ -37,17 +75,23 @@ void run_attach(pid_t tracee)
         }
         icounter++;
     } while(1);
+
     printf("[%d]: executed %d instructions\n", tracee, icounter);
     
 }
 
 int main(int argc, char** argv) 
 {
-    if (argc < 2) {
+    pid_t tracee; 
+    unsigned long map_address;
+
+    if (argc < 3) {
         printf("Missing argument target PID\n");
         exit(1);
     }
 
-    pid_t tracee = atoi(argv[1]);
-    run_attach(tracee);
+    tracee = atoi(argv[1]);
+    map_address = (unsigned long) strtol(argv[2], NULL, 16);
+
+    run_attach(tracee, map_address);
 }
